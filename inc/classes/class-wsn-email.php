82f834de7c779acd480a85fb035501bd30977e33
Nico Mollet <nico.mollet@gmail.com>
2021-10-22 11:11:33 +0200

    Fix translations

--- inc/classes/class-wsn-email.php
+++ inc/classes/class-wsn-email.php
@@ -81,12 +81,9 @@ if (!class_exists('WSN_Email')) {
          */
         public function trigger($users, $product_id)
         {
-			error_log('trigger');
-			error_log(print_r($users, true));
 
-            // ["one@gmail.com", "two@gmail.com", "three@gmail.com"] à remplacer par $users
-            //$this->users = ["nico.mollet@gmail.com", "nmollet@thalasso-saintmalo.com"];
             $this->users = $users;
+
             // get the product
             $this->product = wc_get_product($product_id);
 
@@ -104,7 +101,6 @@ if (!class_exists('WSN_Email')) {
             $header = $this->get_headers() . "\r\n";
 
             // send email
-            //implode( ',', $users )
             $response = $this->send($this->get_from_address(), $this->get_subject(), $this->get_content(), $header, $this->get_attachments());
 
             // Return true in wsn_email_send_response.
@@ -113,25 +109,6 @@ if (!class_exists('WSN_Email')) {
             }
 
         }
-
-
-//        /**
-//         * returnUsers Email function
-//         *
-//         * @access public
-//         * @param mixed $users Waitlist users array.
-//         * @return array
-//         * @since 1.0.0
-//         *
-//         */
-//        public function returnUsers($users)
-//        {
-//
-//            return $users;
-//        }
-
-
-
 
         /**
          * Get the html content of the email.
@@ -182,31 +159,8 @@ if (!class_exists('WSN_Email')) {
 
         public function get_headers()
         {
-
-
-            $header = 'Content-Type: ' . $this->get_content_type() . "\r\n";
-
-            if (in_array($this->id, array('new_order', 'cancelled_order', 'failed_order'), true)) {
-                if ($this->object && $this->object->get_billing_email() && ($this->object->get_billing_first_name() || $this->object->get_billing_last_name())) {
-                    $header .= 'Reply-To: ' . $this->object->get_billing_first_name() . ' ' . $this->object->get_billing_last_name() . ' <' . $this->object->get_billing_email() . ">\r\n";
-                }
-            } elseif ($this->get_from_address() && $this->get_from_name()) {
-	            //$arrayClients = array('04@test.com', '05@test.com', '06@test.com');
-
-	            //$clientsSeparated = implode(",",$arrayClients);
-	            $header .= 'Reply-To: ' . $this->get_from_name() . ' <' . $this->get_from_address() . ">\r\n";
-	            //$header .= 'cc: 01@test.com, 02@test.com , 03@test.com'."\r\n";
-	            //$header.= 'cc: '.$clientsSeparated."\r\n";
-	            //$header.= 'cc: '.$this->get_recipient() ."\r\n";
-	            //$header.= 'cc: '.implode(",",wsn_get_waitlist)."\r\n";
-	            //$header.= 'bcc: 01@test.com, 02@test.com , 03@test.com'."\r\n";
-
-	            $header .= 'bcc :' . implode( ",", $this->users ) . "\r\n";
-
-            }
-
-			error_log('$header');
-	        error_log(print_r($header, true));
+            $header = parent::get_headers();
+	        $header .= 'bcc :' . implode( ",", $this->users ) . "\r\n";
 
 	        return apply_filters('woocommerce_email_headers', $header, $this->id, $this->object, $this);
         }
@@ -214,5 +168,3 @@ if (!class_exists('WSN_Email')) {
 
     }
 }
-
-//end if()
